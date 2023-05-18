a5d8c9fb5e37820c089b2c7d2b2ff495f334ea51
zsefvcx <zsefvcx@list.ru>
2023-05-18 16:39:47 +0300

    task01 flutter finish

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,20 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_svg/flutter_svg.dart';
+
+
+const List<String> _iconNames = <String>[
+  'assets/img/Gerald_G_Creation_Days_Numbers.svg',
+  'assets/img/Gerald_G_Creation_Days_Numbers_1.svg',
+  'assets/img/Gerald_G_Creation_Days_Numbers_2.svg',
+];
+
+const List<String> _uriNames = <String>[
+  'http://upload.wikimedia.org/wikipedia/commons/0/02/SVG_logo.svg',
+  'https://dev.w3.org/SVG/tools/svgweb/samples/svg-files/410.svg',
+  'https://upload.wikimedia.org/wikipedia/commons/b/b4/Chess_ndd45.svg',
+];
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,46 +22,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      title: 'Flutter Task 01',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Task 01 Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -55,71 +47,123 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final double width = 200;
+  final double height = 200;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
+
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: OverflowBox(
+        minWidth: 0,
+        maxWidth: double.infinity,
+        minHeight: 0,
+        maxHeight: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('Добавьте в assets svg изображение и выведите его с помощью этого виджета.'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: width,
+                      height: height,
+                      child: SvgPicture.asset(
+                        fit: BoxFit.contain,
+                        _iconNames[0],
+                        colorFilter: ColorFilter.mode(
+                          Colors.blueGrey[50] ?? Colors.blueGrey,
+                          BlendMode.srcIn,
+                        ),
+                        matchTextDirection: true,
+                      ),
+                    ),
+                    SizedBox(
+                      width: width,
+                      height: height,
+                      child: SvgPicture.asset(
+                        fit: BoxFit.contain,
+                        _iconNames[1],
+                        colorFilter: ColorFilter.mode(
+                          Colors.blueGrey[50] ?? Colors.blueGrey,
+                          BlendMode.srcIn,
+                        ),
+                        matchTextDirection: true,
+                      ),
+                    ),
+                    SizedBox(
+                      width: width,
+                      height: height,
+                      child: SvgPicture.asset(
+                        fit: BoxFit.contain,
+                        _iconNames[2],
+                        colorFilter: ColorFilter.mode(
+                          Colors.blueGrey[50] ?? Colors.blueGrey,
+                          BlendMode.srcIn,
+                        ),
+                        matchTextDirection: true,
+                      ),
+                    ),
+
+
+                  ],
+                ),
+                const Text('С помощью виджета загрузите изображение из сети и отобразите это изображение на экране.'),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: width,
+                      height: height,
+                      child: SvgPicture.network(
+                        fit: BoxFit.contain,
+                        _uriNames[0],
+                        placeholderBuilder: (BuildContext context) => Container(
+                          padding: const EdgeInsets.all(30.0),
+                          child: const CircularProgressIndicator(),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: width,
+                      height: height,
+                      child: SvgPicture.network(
+                        fit: BoxFit.contain,
+                        _uriNames[1],
+                        placeholderBuilder: (BuildContext context) => Container(
+                          padding: const EdgeInsets.all(30.0),
+                          child: const CircularProgressIndicator(),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      width: width,
+                      height: height,
+                      child: SvgPicture.network(
+                        fit: BoxFit.contain,
+                        _uriNames[2],
+                        placeholderBuilder: (BuildContext context) => Container(
+                          padding: const EdgeInsets.all(30.0),
+                          child: const CircularProgressIndicator(),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
